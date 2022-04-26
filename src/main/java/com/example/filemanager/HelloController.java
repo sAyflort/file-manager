ecd39312407738ad5d7491807cf3dff985c768ee
sAyflort <neefe.saratan.life.life@gmail.com>
2022-04-26 23:42:44 +0300

    error

--- src/main/java/com/example/filemanager/HelloController.java
+++ src/main/java/com/example/filemanager/HelloController.java
@@ -1,12 +1,15 @@
 package com.example.filemanager;
 
+import javafx.beans.property.SimpleStringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TextField;
 
 import javafx.scene.layout.VBox;
@@ -15,8 +18,13 @@ import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class HelloController{
+
+public class HelloController implements Initializable {
+    @FXML
+    VBox leftTable, rightTable;
     @FXML
     private VBox authGUI;
     @FXML
@@ -34,10 +42,17 @@ public class HelloController{
         authGUI.setManaged(false);
         fmGUI.setVisible(true);
         fmGUI.setManaged(true);
+
     }
 
     public void reg(ActionEvent actionEvent) throws Exception {
 
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    //   TableColumn<FileInfo, String> fileNameColumn = new TableColumn<>("Тип");
+    //   fileNameColumn.setCellFactory(param -> new SimpleStringProperty(param.));
+
+    }
 }
